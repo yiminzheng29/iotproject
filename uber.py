ffed20b72ebef6d085d2c07fac449cc32645ac0a
yiminzheng29 <yiminzheng29@gmail.com>
2024-03-02 20:49:20 +0800

    update2

--- uber.py
+++ uber.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
-from IPython.display import display
+#from IPython.display import display
 
 
 st.title('Yoga 101')
